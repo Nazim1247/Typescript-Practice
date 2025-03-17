508b1608661a31d96d3c81c006ba13ba360be8fb
nazim uddin <mdnajim1247@gmail.com>
2025-03-17 22:47:20 +0600

    tuple type practice done

---
+++ tuple-type/tuple-type.js
@@ -0,0 +1,6 @@
+// mixed data key, value
+var user;
+user = [101, 'nazim'];
+user.push(102, 'Uddin');
+console.log(user);
+console.log(user[1]);
---
+++ tuple-type/tuple-type.ts
@@ -0,0 +1,9 @@
+// mixed data key, value
+
+let user: [number, string];
+user = [101, 'nazim'];
+
+user.push(102, 'Uddin')
+
+console.log(user);
+console.log(user[1]);
