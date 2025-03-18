d5a1fcc0d23abcf21f1a861708a9da11c495f2dd
nazim uddin <mdnajim1247@gmail.com>
2025-03-18 21:25:10 +0600

    abstract class decleared

---
+++ class-typescript/abstract-class.ts
@@ -0,0 +1,33 @@
+abstract class User{
+    userName: string;
+    age: number;
+
+    constructor(userName: string, age: number){
+        this.userName = userName;
+        this.age = age;
+    }
+
+    abstract display(): void;
+}
+
+class Student extends User{
+    studentId: number;
+
+    constructor(userName: string, age: number, studentId: number){
+        super(userName, age);
+        this.studentId = studentId;
+        
+    }
+    display(): void{
+        console.log(this.userName, this.age, this.studentId);
+    }
+}
+
+let student1 = new Student('Rakib', 32, 10101);
+student1.display();
+
+// let user1 = new User('Nazim Uddin', 25);
+// user1.display();
+
+// let user2 = new User('Nazim', 20);
+// user2.display();
