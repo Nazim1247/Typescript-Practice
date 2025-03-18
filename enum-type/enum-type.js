50c6b03d198c3335561a1562dba9e7e18dc5b1f4
nazim uddin <mdnajim1247@gmail.com>
2025-03-18 12:28:01 +0600

    any-type practice done

---
+++ enum-type/enum-type.js
@@ -0,0 +1,9 @@
+// enum store constant , duplicate value is not allowed here
+// numeric enum
+var RequestType;
+(function (RequestType) {
+    RequestType[RequestType["readData"] = 1] = "readData";
+    RequestType[RequestType["saveData"] = 2] = "saveData";
+    RequestType[RequestType["deleteData"] = 3] = "deleteData";
+})(RequestType || (RequestType = {}));
+console.log(RequestType);
---
+++ enum-type/enum-type.ts
@@ -0,0 +1,10 @@
+// enum store constant , duplicate value is not allowed here
+
+// numeric enum
+enum RequestType{
+    readData=1,
+    saveData,
+    deleteData,
+}
+
+console.log(RequestType);
